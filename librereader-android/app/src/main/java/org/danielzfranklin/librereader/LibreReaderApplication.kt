2431489f2dc512fb6979392faacc3e723088e394
Daniel Franklin <daniel@danielzfranklin.org>
2021-02-07 19:43:21 +0000

    Show line number in log statements

--- librereader-android/app/src/main/java/org/danielzfranklin/librereader/LibreReaderApplication.kt
+++ librereader-android/app/src/main/java/org/danielzfranklin/librereader/LibreReaderApplication.kt
@@ -9,6 +9,12 @@ class LibreReaderApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         Repo.initialize(this)
-        Timber.plant(Timber.DebugTree())
+        Timber.plant(LogDebugTree())
+    }
+
+    class LogDebugTree : Timber.DebugTree() {
+        override fun createStackElementTag(element: StackTraceElement): String? {
+            return "(${element.fileName}:${element.lineNumber})#${element.methodName}"
+        }
     }
 }
