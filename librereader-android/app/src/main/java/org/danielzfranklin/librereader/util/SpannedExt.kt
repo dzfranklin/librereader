8c3e13390bcbfa234c59c756c2788af492305db1
Daniel Franklin <daniel@danielzfranklin.org>
2021-02-07 21:59:01 +0000

    Improve representation

--- librereader-android/app/src/main/java/org/danielzfranklin/librereader/util/SpannedExt.kt
+++ librereader-android/app/src/main/java/org/danielzfranklin/librereader/util/SpannedExt.kt
@@ -1,36 +1,50 @@
 package org.danielzfranklin.librereader.util
 
 import android.text.Spanned
-import kotlin.math.ceil
-import kotlin.math.floor
 
 fun Spanned.toInspectString(): String {
     val spans = getSpans(0, length, Object::class.java)
-        .joinToString("\n") { span ->
+    val spanText = if (spans.isEmpty()) {
+        "No spans"
+    } else {
+        spans.joinToString("\n") { span ->
             val text = try {
                 elideLongText(substring(getSpanStart(span)..getSpanEnd(span)))
             } catch (e: IndexOutOfBoundsException) {
-                "{{Out of bounds: ${e.message}}}"
+                "Out of bounds: ${e.message}"
             }
             val cls = span.`class`
-            val fields = cls.declaredFields
-                .joinToString(", ") {
-                    it.isAccessible = true
-                    "${it.name} = ${it.get(span)}"
-                }
-            "${cls.simpleName}($fields)  [${text}]"
-        }
 
-    return "Spanned inspection: ${elideLongText(toString())}\n---\n$spans"
+            val fields = cls.methods
+                .filter {
+                    it.parameterCount == 0 &&
+                            it.name.startsWith("get") &&
+                            !it.name.endsWith("Internal") &&
+                            arrayOf(
+                                "getClass",
+                                "getUnderlying",
+                                "getSpanTypeId"
+                            ).indexOf(it.name) == -1
+                }
+                .joinToString(", ") {
+                    "${it.name.substring(3 until it.name.length)} = ${it.invoke(span)}"
+                }
+
+            "* ${cls.simpleName} [${text}] ($fields)"
+        }
+    }
+
+    return "Spanned inspection:\n${elideLongText(toString())}\n---\n$spanText"
 }
 
-private fun elideLongText(text: String): String {
-    val overage = text.length - 100
-    if (overage > 0) {
-        val start = text.substring(0..floor(overage.toFloat() / 2f).toInt())
-        val end =
-            text.substring(text.length - ceil(overage.toFloat() / 2f).toInt(), text.length)
-        return "${start}...${end}"
+private const val maxLength = 200
+
+private fun elideLongText(fullText: String): String {
+    var text = fullText.replace("\n", "\\n")
+    if (text.length > maxLength) {
+        val start = text.substring(0..maxLength / 2)
+        val end = text.substring(text.length - maxLength / 2, text.length)
+        text = "${start}...${end}"
     }
     return text
 }
